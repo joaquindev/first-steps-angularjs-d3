e5980a5e20ae3a3ba0a58931a402011b6501fe8a
JoaquinDev <joaquin@isla.io>
2014-01-23 11:29:30 +0100

    Now D3 code is inside our directive
    Now D3 code is inside our directive. We turn on AngularJS machinery
    and we now have our D3 code inside our directive.
    
    Contact: joaquin@isla.io

--- app.js
+++ app.js
@@ -2,10 +2,36 @@ var app = angular.module("myApp", [])
 
 app.directive('donutChart', function(){
    function link(scope, el, attr){
-     d3.select(el[0]).append('svg')
-   }
+     //d3.select(el[0]).append('svg')
+      var data = [82, 32, 63, 42]
+      var color = d3.scale.category10() //giving it some color
+      //var el = d3.select('div').node()
+      var el = el[0]
+      var width = el.clientWidth //dinamically changing its size
+      var height = el.clientHeight
+      var min = Math.min(width, height)
+      var pie = d3.layout.pie().sort(null)
+      var arc = d3.svg.arc().
+            outerRadius(min / 2 * 0.9).
+            innerRadius(min / 2 * 0.5)
+      var svg = d3.select(el).append('svg').
+            attr({width: width, height: height}).
+            append('g').
+            attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')')
+
+      //add the path for each arc slice
+      svg.selectAll('path').data(pie(data)). 
+          enter().append('path').
+          style('stroke', 'white').
+          attr('d', arc).
+          attr('fill', function(d,i){ return color(i) })
+         }
    return {
-    link: link
+    link: link, 
+    restrict: 'E'
    } 
 })
 
+/*
+
+*/
